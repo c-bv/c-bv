d7bbf82884fc39fec1c9cf0e603b8d2fd7b2b5b3
c-bv <me@c-bv.dev>
2024-02-21 11:03:07 +0100

    Update home page title

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,7 @@
 export const Home = () => {
     return (
         <>
-            <h1 className='mb-4 text-3xl font-bold'>Hello World</h1>
-            <p className='text-lg'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
-            </p>
+            <h1 className='mb-4 text-3xl font-bold'>Home page</h1>
         </>
     );
 };
